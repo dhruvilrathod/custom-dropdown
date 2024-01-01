03f9ec04c621321f4a0a2ab4d7818388ddf64768
dhruvilrathod <dhruvilrathod2001@gmail.com>
2024-01-01 18:51:09 +0530

    added basic functionality for dropdown tree utility

--- utility/FolderTree.ts
+++ utility/FolderTree.ts
@@ -9,20 +9,15 @@ interface ITreeFieldsSrcConfigurations {
     dataParentUniqueIdsSrc?: string;
     dataDisabledSrc?: string;
     dataReadOnlySrc?: string;
+    dataSearchFieldsSrc?: string[];
 }
-
-interface ITreeUtility {
-
-}
-
-interface TreeUtility {
-    propertyAccess: typeof TreeUtility.propertyAccess;
-    traverseAllNodes: typeof TreeUtility.traverseAllNodes;
-}
-
 
 interface IOperatorFunction {
     (node: TreeNode): void;
+}
+
+interface IStopperFunction {
+    (node: TreeNode): boolean;
 }
 
 interface ITree {
@@ -32,7 +27,7 @@ interface ITree {
     postOrderTraversal(node?: TreeNode): Generator<TreeNode>;
     insert(dataUniqueFieldValue: string | number, value: any): boolean;
     remove(key: string | number): boolean;
-    find(dataUniqueFieldValue: string | number): TreeNode | undefined;
+    findNodeFromId(dataUniqueFieldValue: string | number): TreeNode | undefined;
 }
 
 interface IDropDownTreeConfig extends ITreeFieldsSrcConfigurations {
@@ -48,7 +43,7 @@ interface IDropDownTreeConfig extends ITreeFieldsSrcConfigurations {
     isSelectAllAvailable?: boolean;
     isMultipleLevel?: boolean;
     isAsynchronouslyExpandable?: boolean;
-    isHierarchySelectionModificationAllowed?: boolean;
+    isHierarchySelectionModificationAllowed?: boolean; // done
     isCustomAllSelectOption?: boolean;
     minSelectCount?: number;
     maxSelectCount?: number;
@@ -56,6 +51,7 @@ interface IDropDownTreeConfig extends ITreeFieldsSrcConfigurations {
 
 interface IDropDownTree extends ITree {
     selectAll(isReset?: boolean): void;
+    itemSelection(dataUniqueFieldValue: string | number, selectionVal?: boolean): void;
 }
 
 interface ITreeNode {
@@ -73,6 +69,7 @@ interface ITreeNode {
     dataExpandableValue?: boolean;
     dataFavouriteValue?: boolean;
     dataTotalDocsValue?: number;
+    dataSearchFieldsValues: string[];
     children: TreeNode[];
     originalData?: any;
     parent?: TreeNode;
@@ -92,14 +89,13 @@ class TreeNode implements ITreeNode {
     isExpanded?: boolean;
     isChildernLoading?: boolean;
     levelIndex?: number;
-    isPartiallySelected?: boolean;
-    isAllChildrenSelected?: boolean;
     dataUniqueFieldValue: string | number;
     dataVisibleNameValue: string;
     dataTooltipValue?: string;
     dataExpandableValue?: boolean;
     dataFavouriteValue?: boolean;
     dataTotalDocsValue?: number;
+    dataSearchFieldsValues: string[];
     children: TreeNode[];
     originalData?: any;
     parent?: TreeNode;
@@ -109,28 +105,29 @@ class TreeNode implements ITreeNode {
     onSelect!: Function;
     onDeselect!: Function;
 
-    constructor(value: any, config: ITreeFieldsSrcConfigurations, parent?: TreeNode, levelIndex: number = 0, nodeType = 2) {        
+    constructor(value: any, config: ITreeFieldsSrcConfigurations, parent?: TreeNode, levelIndex: number = 0, nodeType = 2) {
 
         // fields which are related to identity of the node, possibly acquiring no changes at runtime
-        this.isDisabled = config.dataDisabledSrc ? TreeUtility.propertyAccess(value, config.dataDisabledSrc) : false;
-        this.isReadOnly = config.dataReadOnlySrc ? TreeUtility.propertyAccess(value, config.dataReadOnlySrc) : false;
+        this.isDisabled = config.dataDisabledSrc !== undefined ? TreeUtility.propertyAccess(value, config.dataDisabledSrc) : false;
+        this.isReadOnly = config.dataReadOnlySrc !== undefined ? TreeUtility.propertyAccess(value, config.dataReadOnlySrc) : false;
         this.dataUniqueFieldValue = TreeUtility.propertyAccess(value, config.dataUniqueFieldSrc);
         this.dataVisibleNameValue = TreeUtility.propertyAccess(value, config.dataVisibleNameSrc);
-        this.dataTooltipValue = config.dataTooltipSrc ? TreeUtility.propertyAccess(value, config.dataTooltipSrc) : TreeUtility.propertyAccess(value, config.dataVisibleNameSrc);
-        this.dataExpandableValue = config.dataExpandableSrc ? TreeUtility.propertyAccess(value, config.dataExpandableSrc) : false;
-        this.dataFavouriteValue = config.dataFavouriteSrc ? TreeUtility.propertyAccess(value, config.dataFavouriteSrc) : false;
-        this.dataTotalDocsValue = config.dataTotalDocsSrc ? TreeUtility.propertyAccess(value, config.dataTotalDocsSrc) : 0;
+        this.dataTooltipValue = config.dataTooltipSrc !== undefined ? TreeUtility.propertyAccess(value, config.dataTooltipSrc) : TreeUtility.propertyAccess(value, config.dataVisibleNameSrc);
+        this.dataExpandableValue = config.dataExpandableSrc !== undefined ? TreeUtility.propertyAccess(value, config.dataExpandableSrc) : false;
+        this.dataFavouriteValue = config.dataFavouriteSrc !== undefined ? TreeUtility.propertyAccess(value, config.dataFavouriteSrc) : false;
+        this.dataTotalDocsValue = config.dataTotalDocsSrc !== undefined ? TreeUtility.propertyAccess(value, config.dataTotalDocsSrc) : 0;
+        this.dataSearchFieldsValues = config.dataSearchFieldsSrc && config.dataSearchFieldsSrc.length > 0 ? config.dataSearchFieldsSrc : [];
         this.originalData = value;
 
         // fields which are used to manage UI states and functionalities at the runtime
         this.isSelected = value.isSelected || false;
         this.isExpanded = value.isExpanded || false;
         this.isChildernLoading = value.isChildernLoading || false;
-        this.isPartiallySelected = value.isPartiallySelected || false;
-        this.isAllChildrenSelected = value.isAllChildrenSelected || false;
+        // this.isPartiallySelected = value.isPartiallySelected || false;
+        // this.isAllChildrenSelected = value.isAllChildrenSelected || false;
         this.levelIndex = parent && parent.levelIndex !== undefined ? parent.levelIndex + 1 : levelIndex;
         this.parent = parent;
-        this.children = [];        
+        this.children = [];
     }
 
     get isLeaf(): boolean {
@@ -139,6 +136,14 @@ class TreeNode implements ITreeNode {
 
     get hasChildren(): boolean {
         return !this.isLeaf;
+    }
+
+    get isPartiallySelected(): boolean {
+        return this.children.length > 0 ? this.children.some((val: TreeNode) => val.isSelected === false) && this.children.reduce<boolean>((acc: boolean, val: TreeNode) => val.isSelected !== undefined ? val.isSelected || acc : false, false) : false;
+    }
+
+    get isAllChildrenSelected(): boolean {
+        return this.children.length > 0 ? this.children.some((val: TreeNode) => val.isSelected === false) ? false : true : false;
     }
 }
 
@@ -193,15 +198,41 @@ class Tree implements ITree {
         return false;
     }
 
-    find(dataUniqueFieldValue: string | number): TreeNode | undefined {
+    findNodeFromId(dataUniqueFieldValue: string | number): TreeNode | undefined {
         for (let node of this.preOrderTraversal()) {
             if (node.dataUniqueFieldValue === dataUniqueFieldValue) return node;
         }
         return undefined;
     }
+
+    findNodes(searchValue: string): TreeNode[] {
+
+        if (!searchValue || typeof searchValue !== "string" || searchValue === "" || searchValue === null) {
+            return [];
+        }
+
+        let searchNodes: TreeNode[] = [];
+        const searchQuery = searchValue.toString().toLowerCase().trim();
+
+        for (let node of this.preOrderTraversal()) {
+            let dataSearchFieldsValuesLen = node.dataSearchFieldsValues.length;
+            if(dataSearchFieldsValuesLen > 0) {
+                for (let i = 0; i < dataSearchFieldsValuesLen; i++) {
+                    console.log(node.dataSearchFieldsValues[i]);
+                    
+                    let accessedValue = TreeUtility.propertyAccess(node.originalData, node.dataSearchFieldsValues[i]).toString().toLowerCase().trim();
+                    if(!(!accessedValue || typeof accessedValue !== "string" || accessedValue === "" || accessedValue === null) && accessedValue.includes(searchQuery)) {
+                        searchNodes.push(node);
+                    }
+                }
+            }
+        }
+
+        return searchNodes;
+    }
 }
 
-class TreeUtility implements ITreeUtility {
+class TreeUtility {
 
     constructor() { }
 
@@ -221,44 +252,102 @@ class TreeUtility implements ITreeUtility {
                 value = value[pathArr[k]];
             }
         }
+        console.log(dataObj);
+        
         return value;
     }
 
-    static traverseAllNodes(tree: Tree, operatorFunction?: IOperatorFunction): void {
-        for (let node of tree.preOrderTraversal()) {
+    static traverseAllNodesPreOrder(tree: Tree, operatorFunction: IOperatorFunction, startNode?: TreeNode, stopperFunction?: IStopperFunction, stopWithLastOperationPerformed = true): void {
+
+        for (let node of tree.preOrderTraversal(startNode)) {
+            if (stopperFunction && stopperFunction(node)) {
+                stopWithLastOperationPerformed && operatorFunction && operatorFunction(node);
+                return;
+            }
             operatorFunction && operatorFunction(node);
-            console.log(node.dataUniqueFieldValue, node.dataVisibleNameValue, node.isSelected);
+        }
+    }
+
+    static traverseAllNodesPostOrder(tree: Tree, operatorFunction: IOperatorFunction, startNode?: TreeNode, stopperFunction?: IStopperFunction, stopWithLastOperationPerformed = true): void {
+        for (let node of tree.postOrderTraversal()) {
+            if (stopperFunction && stopperFunction(node)) {
+                stopWithLastOperationPerformed && operatorFunction && operatorFunction(node);
+                return;
+            }
+            operatorFunction && operatorFunction(node);
         }
     }
 }
 
 class DropdownTree extends Tree implements IDropDownTree {
 
+    config: IDropDownTreeConfig;
 
     constructor(config: IDropDownTreeConfig, rootValue: any) {
-
         super(config, rootValue);
 
+        this.config = config;
+
+        this.config.isRequired = config.isRequired !== undefined ? config.isRequired : false;
+        this.config.isDisabled = config.isDisabled !== undefined ? config.isDisabled : false;
+        this.config.isSingularInput = config.isSingularInput !== undefined ? config.isSingularInput : false;
+        this.config.isReadonly = config.isReadonly !== undefined ? config.isReadonly : false;
+        this.config.isCustomInputAllowed = config.isCustomInputAllowed !== undefined ? config.isCustomInputAllowed : false;
+        this.config.isSearchAllowed = config.isSearchAllowed !== undefined ? config.isSearchAllowed : false;
+        this.config.isAsynchronousSearch = config.isAsynchronousSearch !== undefined ? config.isAsynchronousSearch : false;
+        this.config.isClientSideSearchAllowed = config.isClientSideSearchAllowed !== undefined ? config.isClientSideSearchAllowed : false;
+        this.config.isResetOptionVisible = config.isResetOptionVisible !== undefined ? config.isResetOptionVisible : false;
+        this.config.isSelectAllAvailable = config.isSelectAllAvailable !== undefined ? config.isSelectAllAvailable : false;
+        this.config.isMultipleLevel = config.isMultipleLevel !== undefined ? config.isMultipleLevel : false;
+        this.config.isAsynchronouslyExpandable = config.isAsynchronouslyExpandable !== undefined ? config.isAsynchronouslyExpandable : false;
+        this.config.isHierarchySelectionModificationAllowed = config.isHierarchySelectionModificationAllowed !== undefined ? config.isHierarchySelectionModificationAllowed : false;
+        this.config.isCustomAllSelectOption = config.isCustomAllSelectOption !== undefined ? config.isCustomAllSelectOption : false;
+        this.config.minSelectCount = config.minSelectCount !== undefined ? config.minSelectCount : -1;
+        this.config.maxSelectCount = config.maxSelectCount !== undefined ? config.maxSelectCount : -1;
     }
 
 
-    public selectAll(isReset: boolean = false): void {        
-        TreeUtility.traverseAllNodes(this, (node: TreeNode) => {
+    public selectAll(isReset: boolean = false): void {
+        TreeUtility.traverseAllNodesPreOrder(this, (node: TreeNode) => {
             node.isSelected = !isReset;
-            node.children && node.children.length > 0 && (node.isAllChildrenSelected = !isReset);
         });
     }
 
     public getCurrentSelectedNodesArray(): Array<TreeNode> {
-        let selectedNodes: TreeNode[] = []
-        
-        TreeUtility.traverseAllNodes(this, (node: TreeNode) => {
-            if(node.levelIndex && node.levelIndex > 0 && node.isSelected === true) {
+        let selectedNodes: TreeNode[] = [];
+
+        TreeUtility.traverseAllNodesPreOrder(this, (node: TreeNode) => {
+            if (!this.config.isHierarchySelectionModificationAllowed && node.levelIndex !== undefined && node.levelIndex > 0 && node.isSelected === true) {
                 selectedNodes.push(node);
             }
-        });
+            else if (this.config.isHierarchySelectionModificationAllowed && node.levelIndex !== undefined && node.levelIndex > 0 && node.isAllChildrenSelected === true) {
+
+                selectedNodes.push(node);
+            }
+            else if (this.config.isHierarchySelectionModificationAllowed && node.levelIndex !== undefined && node.levelIndex === 0 && node.isAllChildrenSelected === true) {
+                selectedNodes.push(...this.root.children);
+            }
+        }, undefined, (node: TreeNode) => this.config.isHierarchySelectionModificationAllowed === true && node.isAllChildrenSelected === true);
 
         return selectedNodes;
+    }
+
+    public itemSelection(dataUniqueFieldValue: string | number, selectionVal: boolean = true): void {
+        let currentNode = this.findNodeFromId(dataUniqueFieldValue);
+
+        if (currentNode) {
+            currentNode.isSelected = selectionVal;
+        }
+
+        if (this.config.isHierarchySelectionModificationAllowed) {
+            TreeUtility.traverseAllNodesPreOrder(this, (node: TreeNode) => {
+                node.isSelected = selectionVal;
+            }, currentNode);
+
+            TreeUtility.traverseAllNodesPostOrder(this, (node: TreeNode) => {
+                node.isSelected = node.children.length > 0 ? node.children.some((val: TreeNode) => val.isSelected === false) ? false : true : node.isSelected;
+            });
+        }
     }
 
 }
@@ -266,22 +355,42 @@ class DropdownTree extends Tree implements IDropDownTree {
 let data: any[] = [
     {
         folder_id: "1",
-        folderName: "aaa"
+        folderName: "aaa",
+        folderPath: "myFolder/ff-asssffdf",
     },
     {
         folder_id: "2",
-        folderName: "bbb"
+        folderName: "bbb sdffsd",
+        folderPath: "myFolder/ff-asssffdf",
+    }
+];
+
+let data1: any[] = [
+    {
+        folder_id: "11",
+        folderName: "1aaa daddcc",
+        folderPath: "myFolder/ff-dfdfd",
+    }
+];
+
+let data11: any[] = [
+    {
+        folder_id: "111",
+        folderName: "11aaa dffrfrf",
+        folderPath: "myFolder/ff-sdfsd",
     }
 ];
 
 let data2: any[] = [
     {
         folder_id: "21",
-        folderName: "2aaa"
+        folderName: "2aaa ffgfgf",
+        folderPath: "myFolder/ff-fffffff",
     },
     {
         folder_id: "22",
-        folderName: "2bbb"
+        folderName: "2bbb ffgnfjgnbf",
+        folderPath: "myFolder/ff-fffff",
     }
 ];
 
@@ -289,16 +398,50 @@ let data2: any[] = [
 let mySection: DropdownTree = new DropdownTree({
     dataUniqueFieldSrc: "folder_id",
     dataVisibleNameSrc: "folderName",
+    dataTooltipSrc: "folderPath",
+    dataSearchFieldsSrc: ["folderName", "folderPath"],
+    isHierarchySelectionModificationAllowed: false
 }, {
     folder_id: "0",
     folderName: "Folders",
+    folderPath: "myFolder/ff-asssffdf",
 });
 
 data.forEach((val) => mySection.insert("0", val));
 data2.forEach((val) => mySection.insert("2", val));
 
-mySection.find("1") && (mySection.find("1")!.isSelected = true);
-mySection.find("21") && (mySection.find("21")!.isSelected = true);
-console.log(mySection.getCurrentSelectedNodesArray().length);
+data1.forEach((val) => mySection.insert("1", val));
+data11.forEach((val) => mySection.insert("11", val));
 
-// console.log(mySection.find("2")?.dataVisibleNameValue, mySection.find("2")?.isSelected);
+// mySection.findNodeFromId("1") && (mySection.findNodeFromId("1")!.isSelected = false);
+// mySection.findNodeFromId("21") && (mySection.findNodeFromId("21")!.isSelected = true);
+// mySection.findNodeFromId("21") && (mySection.findNodeFromId("22")!.isSelected = true);
+
+// mySection.itemSelection("2", false);
+
+// mySection.itemSelection("21", true);
+// mySection.itemSelection("22", true);
+
+// console.log(mySection.findNodeFromId("21")?.isSelected);
+// console.log(mySection.findNodeFromId("22")?.isSelected);
+// console.log(mySection.findNodeFromId("2")?.isAllChildrenSelected, "sdf");
+// console.log(mySection.findNodeFromId("21")?.isSelected);
+// console.log(mySection.findNodeFromId("22")?.isSelected);
+
+// mySection.selectAll();
+
+// console.log(mySection.getCurrentSelectedNodesArray()[0].dataVisibleNameValue);
+// console.log(mySection.getCurrentSelectedNodesArray()[1].dataVisibleNameValue);
+// console.log(mySection.getCurrentSelectedNodesArray()[0].dataVisibleNameValue);
+
+// mySection.itemSelection("111");
+// console.log(mySection.findNodeFromId("111")?.dataVisibleNameValue, mySection.findNodeFromId("111")?.isSelected);
+// console.log(mySection.findNodeFromId("11")?.dataVisibleNameValue, mySection.findNodeFromId("11")?.isSelected);
+// console.log(mySection.findNodeFromId("11")?.dataVisibleNameValue, mySection.findNodeFromId("11")?.isSelected);
+// console.log(mySection.findNodeFromId("1")?.dataVisibleNameValue, mySection.findNodeFromId("1")?.isSelected);
+// console.log(mySection.getCurrentSelectedNodesArray().length);
+
+// console.log(mySection.findNodeFromId("1")?.dataSearchFieldsValues);
+console.log(mySection.findNodes("2bbb").length);
+
+
